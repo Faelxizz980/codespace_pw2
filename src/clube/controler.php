fa7c421b94d0fc177c113534a65b4dd1825b0b7a
fael <shaolindopneu68@gmail.com>
2025-08-25 11:41:27 +0000

    ss

--- src/clube/controler.php
+++ src/clube/controler.php
@@ -1,6 +1,6 @@
 <?php
 $host = 'db'; // Nome do serviço do banco de dados no docker-compose
-$db   = 'campeonato_futebol'; // Nome do banco de dados
+$db   = 'campeonato_futbol'; // Nome do banco de dados
 $user = 'root'; // Nome do usuário do banco de dados
 $pass = 'root'; // Senha do usuário do banco de dados
 $charset = 'utf8mb4';
